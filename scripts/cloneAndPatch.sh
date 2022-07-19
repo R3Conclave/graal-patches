aec21701c2cce83232811dc3055e87350bd2a6eb
Filipe Oliveira <filipe.oliveira@r3.com>
2022-07-19 12:01:29 +0100

    Update

--- scripts/cloneAndPatch.sh
+++ scripts/cloneAndPatch.sh
@@ -13,7 +13,7 @@ graal_dir="graal"
 rm -fr "$graal_dir"
 
 # Download Graal
-# Use --filter option to download the minimum about of data
+# Use --filter option to download the minimum amount of data
 # Reference: https://github.blog/2020-12-21-get-up-to-speed-with-partial-clone-and-shallow-clone/
 git clone --filter=tree:0 "$graal_repo" -b "$graal_branch"
 

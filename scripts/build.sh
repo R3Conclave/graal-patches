a162937c7d64f8c4034a136d5679ec7b61fd1028
Filipe Oliveira <filipe.oliveira@r3.com>
2022-07-19 10:26:35 +0100

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,5 @@ set -xeuo pipefail
 script_dir=$(dirname ${BASH_SOURCE[0]})
 source ${script_dir}/build_common.sh
 
-# Kill any lingering Gradle workers
-ps auxwww | grep Gradle | grep -v grep | awk '{ print $2; }' | xargs -r kill || true
-
 # Then run the build
 runDocker ${container_image_graalvm_build} "./gradlew tarGraal --info"
